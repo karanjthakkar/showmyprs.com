34e550ebbd253a173dc074e3579a74b6d102bbd2
Karan Thakkar <karanjthakkar@gmail.com>
2017-02-18 23:32:44 +0530

    go fmt the code

--- models.parsedPrUrl.go
+++ models.parsedPrUrl.go
@@ -1,7 +1,7 @@
 package main
 
 type ParsedPrUrl struct {
-  Owner string `json:"owner"`
-  Repo string `json:"repo"`
-  Number int `json:"pullNumber"`
+	Owner  string `json:"owner"`
+	Repo   string `json:"repo"`
+	Number int    `json:"pullNumber"`
 }
--- models.pr.go
+++ models.pr.go
@@ -1,8 +1,8 @@
 package main
 
 type PullRequest struct {
-  Url string `json:"url"`
-  Title string `json:"title"`
-  State string `json:"state"`
-  RepoUrl string `json:"-"`
+	Url     string `json:"url"`
+	Title   string `json:"title"`
+	State   string `json:"state"`
+	RepoUrl string `json:"-"`
 }
