44224f9494ee918068f4ed5df1632235df87221c
Karan Thakkar <karanjthakkar@gmail.com>
2020-01-19 04:16:31 +0000

    Fix rewrite to functions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,21 @@
 const functions = require('firebase-functions');
 const { getPullRequestsForUser } = require('./github.js');
+const _ = require('lodash');
 
 exports.user = functions.https.onRequest(async (request, response) => {
   try {
-    const data = await getPullRequestsForUser(request.params[0]);
-    response.send(data);
+    const username = _.get(request, 'params[0]', '').split('/')[1];
+    if (username) {
+      const data = await getPullRequestsForUser(username);
+      response.send(data);
+    } else {
+      response.statusCode = 500;
+      response.send({
+        error: 'Please provide a username'
+      });
+    }
   } catch(error) {
+    response.statusCode = 500;
     response.send({
       error: error.toString(),
       stacktrace: error.stack
