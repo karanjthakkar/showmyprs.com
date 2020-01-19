e96036e8dc450b8ddfccbda9c9c831aed64735d4
Karan Thakkar <karanjthakkar@gmail.com>
2020-01-19 03:22:23 +0000

    Remove firebase config from resp

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,11 @@ const { getPullRequestsForUser } = require('./github.js');
 exports.user = functions.https.onRequest(async (request, response) => {
   try {
     const data = await getPullRequestsForUser(request.params[0]);
-    response.send({
-      ...data,
-      config: process.env.FIREBASE_CONFIG
-    });
+    response.send(data);
   } catch(error) {
     response.send({
       error: error.toString(),
-      stacktrace: error.stack,
-      config: process.env.FIREBASE_CONFIG
+      stacktrace: error.stack
     });
   }
 });
